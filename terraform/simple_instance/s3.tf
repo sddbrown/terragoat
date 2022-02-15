8d8b406a1cf644b9aa3eb6d8b9987281898c0f4a
Steve Brown <sddbrown@gmail.com>
2022-02-15 12:43:29 -0800

    Update s3.tf

--- terraform/simple_instance/s3.tf
+++ terraform/simple_instance/s3.tf
@@ -1,1 +1,12 @@
-TEST
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "docking_bay" {
+  bucket_prefix = "docking-bay-storage-"
+
+  tags = {
+    Name                 = "Docking Bay"
+    Environment          = "Dev"
+  }
+}
